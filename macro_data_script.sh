8276484da94dbe75a1af19174851823d1aa8738e
ceschi <emanuele.franceschi@gmail.com>
2018-10-11 23:37:49 +0200

    db twist

---
+++ macro_data_script.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+cd
+
+cd /home/pi/Desktop/R/US_macro_data/
+
+sudo rm -r ./Plots/*.*
+
+sudo git pull 
+
+sudo Rscript main.R
+
+echo ' ' | mailx -s 'TR database and estimates updated' emanuele.franceschi@gmail.com
+
+cd ~
+
+cd /var/www/html/files/
+
+sudo zip -r US_macro_data.zip /home/pi/Desktop/R/US_macro_data/Processed\ data/*.txt
+
+sudo zip data.zip /home/pi/Desktop/R/US_macro_data/Readme_data.pdf
