d2f6c87977e712f6dc9c37b4d85dff270525ddc4
ceschi <emanuele.franceschi@gmail.com>
2018-10-13 01:47:17 +0200

    tex report

--- macro_data_script.sh
+++ macro_data_script.sh
@@ -14,3 +14,5 @@ texliveonly -c ~/Desktop/tex/us_plot/plots.tex
 echo ' ' | mailx -s 'TR database and estimates updated' -A ~/Desktop/R/tex/us_plot/plots.pdf emanuele.franceschi@gmail.com
 
 sudo zip /var/www/html/files/us_macro_data.zip ./Processed\ data/*.txt -j
+
+sudo mv /home/pi/Desktop/tex/us_plot/us_plot.pdf /var/www/html/files/us_plot.pdf
