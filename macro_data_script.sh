03b4bef57331d91e8babbc9369caa2d1ea932e1d
ceschi <emanuele.franceschi@gmail.com>
2018-10-13 15:28:48 +0200

    tex report

--- macro_data_script.sh
+++ macro_data_script.sh
@@ -11,7 +11,7 @@ sudo Rscript main.R
 
 sudo cp ~/Desktop/R/us_macro_data/Plots/*.pdf	~/Desktop/tex/us_plot/
 
-texliveonfly ~/Desktop/tex/us_plot/plots.tex
+sudo texliveonfly ~/Desktop/tex/us_plot/plots.tex
 
 echo ' ' | mailx -s 'TR database and estimates updated' -A ~/Desktop/R/tex/us_plot/plots.pdf emanuele.franceschi@gmail.com
 
