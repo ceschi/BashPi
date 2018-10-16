916d32785a5233a745603359f6e84ee9ca3afc72
ceschi <emanuele.franceschi@gmail.com>
2018-10-16 15:52:45 +0200

    tex report

--- macro_data_script.sh
+++ macro_data_script.sh
@@ -11,14 +11,14 @@ sudo Rscript main.R
 
 sudo cp ~/Desktop/R/us_macro_data/Plots/*.pdf	~/Desktop/tex/us_plot/
 
+sudo zip /var/www/html/files/us_macro_data.zip ./Processed\ data/*.txt -j
+
+sudo mv /home/pi/Desktop/tex/us_plot/plots.pdf /var/www/html/files/us_plots.pdf
+
 cd ~/Desktop/tex/us_plot/
 
 texliveonfly plots.tex
 
 echo ' ' | mailx -s 'TR database and estimates updated' -A ~/Desktop/R/tex/us_plot/plots.pdf emanuele.franceschi@gmail.com
 
-cd ~/Desktop/R/us_macro_data/
-
-sudo zip /var/www/html/files/us_macro_data.zip ./Processed\ data/*.txt -j
-
-sudo mv /home/pi/Desktop/tex/us_plot/plots.pdf /var/www/html/files/us_plots.pdf
+echo 'Script executed - mail received?' > ~/Desktop/db_script.log
