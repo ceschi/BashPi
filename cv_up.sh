7a18c0023b9d0b6d58cc9db758cd507756e673ee
ceschi <emanuele.franceschi@gmail.com>
2018-11-17 13:33:16 +0100

    clean up aux files

--- cv_up.sh
+++ cv_up.sh
@@ -7,3 +7,5 @@ texliveonfly -c xelatex cv_acc_EN.tex
 texliveonfly -c xelatex cv_acc_FR.tex
 
 sudo cp -f cv_acc_*.pdf /var/www/html/files/
+
+sudo rm cv_acc_*.aux cv_acc_*.log  cv_acc_*.out cv_acc_*.synctex.gz
